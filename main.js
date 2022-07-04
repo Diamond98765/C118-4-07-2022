88cab3c8a58ad492d330ec1b8477eb11a350da35
Diamond98765 <104906271+Diamond98765@users.noreply.github.com>
2022-07-04 17:50:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+
+
+function setup() {
+    canvas = createCanvas(280, 280);
+    canvas.center();
+    background("white");
+    canvas.mouseReleased(classifyCanvas);
+    synth = window.speechSynthesis;
+  }
+  
+  function preload() {
+  
+  
+    classifier = ml5.imageClassifier('DoodleNet');
+  }
+  
+  
+  
+  
